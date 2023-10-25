bec2d7a11ef2c4ea85b6d06ac825d6ed5fe258d6
Adrian <adrianx1313@gmail.com>
2023-10-25 00:24:22 -0500

    .

--- dist/tailwind.config.js
+++ dist/tailwind.config.js
@@ -3,6 +3,10 @@
 module.exports = {
     content: ["./views/*.{html,js,ejs}", "./views/**/*.{html,js,ejs}"],
     theme: {
+        colors: {
+            "primary": "#3498db",
+            "black": "#000"
+        },
         extend: {},
     },
     plugins: [],
