ad002fb3bb2f5e388dbfe05625c7e59f10fabc29
Adrian <adrianx1313@gmail.com>
2023-10-25 00:23:21 -0500

    .

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./views/*.{html,js,ejs}", "./views/**/*.{html,js,ejs}"],
   theme: {
+    colors: {
+      "primary": "#3498db",
+      "black": "#000"
+    },
     extend: {},
   },
   plugins: [],
