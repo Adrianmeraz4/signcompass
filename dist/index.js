64729973c24450bcac9b426a9639cc98dbebe817
Adrian <adrianx1313@gmail.com>
2023-11-03 14:47:40 -0500

    Forgotten password btn

--- dist/index.js
+++ dist/index.js
@@ -53,13 +53,13 @@ app.use((0, express_session_1.default)({
     cookie: { maxAge: oneDay },
     resave: false
 }));
+// Define a route to serve the HTML file
 app.get('/Lesson:id', function (req, res) {
     res.render('Lesson' + req.params.id);
 });
 app.get('/Quiz:id', function (req, res) {
     res.render('Quiz' + req.params.id);
 });
-// Define a route to serve the HTML file
 app.get('/', (req, res) => {
     console.log('userid: ', req.session.userId);
     res.render('index', { session: req.session });
